e285843dbdf590f91574b91612c602e8c211c61f
Amruta <amruta.panda.mit@gmail.com>
2024-10-22 18:57:29 +0530

    created an attendance system which updates the realtime date and time on google sheet

--- barcode.py
+++ barcode.py
@@ -1,48 +1,105 @@
 import cv2
+import numpy as np
 from pyzbar.pyzbar import decode
-import pandas as pd
+import matplotlib.pyplot as plt
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
+import time
 
-barcode_data = []
+barcode_database = {
+    "1032220289": "Harsh Chourasia",
+    "1032222392": "Om Gunjal",
+    "1032222399": "Aditya Naik",
+    "1032232290": "Jayesh Sangave",
+    "1032221144": "Rishit Darwade",
+    "1032220740": "Aishwarya Godse",
+    "1032221070": "Sourabh Bhosale",
+    "1032220354": "Parjanya Kilambi",
+    "1032230586": "Shravani Pachpute",
+    "1032220415": "Hrishikesh Ghogle",
+    "1032220559": "Danish Tapia",
+    "1032240120": "Avnish Deshmukh",
+    "1032233454": "Amruta Panda",
+    "1032210608": "Samyak Kharat",
+    "1032211911": "Sujal Bafna",
+    "1032240199": "Vishweshwar Patil",
+    "1032240202": "Aaditya Patil",
+    "1032221888": "Himadri Rajput",
+    "1032212583": "Prithviraj Patil",
+    "1032210315": "Rameshwar Patil",
+    "1032221684": "Rutu Shirke",
+}
 
-cap = cv2.VideoCapture(0)
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("lab-attendance-439409-308fc8515cfc.json", scope)
+client = gspread.authorize(creds)
 
-if not cap.isOpened():
-    print("Error: Could not open webcam.")
-    exit()
+sheet = client.open("Attendance Sheet").sheet1  
 
-print("Press 'q' to quit.")
+cap = cv2.VideoCapture(2)  
+cap.set(3, 640)  
+cap.set(4, 480) 
+
+plt.ion()  
+fig, ax = plt.subplots()
+
+scan_count = 0  
+detected_barcodes = set()  
+last_detection_time = time.time() 
+detection_interval = 2  
 
 while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Error: Could not read frame.")
+    success, img = cap.read()
+    if not success:
         break
 
-    decoded_objects = decode(frame)
-    
-    for obj in decoded_objects:
+    barcodes = decode(img)
+    current_time = time.time()
+
+    for barcode in barcodes:
+        myData = barcode.data.decode("utf-8")
         
-        (x, y, w, h) = obj.rect
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        if myData not in detected_barcodes and (current_time - last_detection_time >= detection_interval):
+            if myData in barcode_database:
+                name = barcode_database[myData]
+                color = (0, 0, 255)
+
+                pts = np.array([barcode.polygon], np.int32)
+                pts = pts.reshape((-1, 1, 2))
+                cv2.polylines(img, [pts], True, color, 5)
+                pts2 = barcode.rect
+                cv2.putText(img, myData, (pts2[0], pts2[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+
+                scan_count += 1
+                current_date = datetime.now().strftime("%Y-%m-%d")
+                current_time_str = datetime.now().strftime("%H:%M:%S")
+
+                next_row = len(sheet.col_values(1)) + 1  
+
+                sheet.update_cell(next_row, 1, next_row - 1)  
+                sheet.update_cell(next_row, 2, name)  
+                sheet.update_cell(next_row, 3, current_date)  
+                
+                if scan_count % 2 == 1:  
+                    sheet.update_cell(next_row, 4, current_time_str)  
+                    sheet.update_cell(next_row, 5, '')  
+                else:  
+                    sheet.update_cell(next_row, 5, current_time_str)  
+
+                detected_barcodes.add(myData)
+                last_detection_time = current_time
+
+    if current_time - last_detection_time >= detection_interval:
+        detected_barcodes.clear()  
+
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
-        barcode_text = obj.data.decode('utf-8')
-        barcode_type = obj.type
-
-        print(f"Detected {barcode_type}: {barcode_text}")
-        barcode_data.append({'Barcode': barcode_text, 'Type': barcode_type, 'Timestamp': datetime.now()})
-
-    cv2.imshow('Barcode Scanner', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    ax.clear()
+    ax.imshow(img_rgb)
+    ax.axis('off')  
+    plt.pause(0.001)  
 
 cap.release()
-cv2.destroyAllWindows()
-
-if barcode_data:
-    df = pd.DataFrame(barcode_data)
-    df.to_excel('scanned_barcodes.xlsx', index=False)
-    print("Data saved to scanned_barcodes.xlsx")
-else:
-    print("No barcodes scanned.")
+plt.ioff()  
+plt.show()
--- test_pyzbar.py
+++ test_pyzbar.py
@@ -2,7 +2,7 @@ import cv2
 from pyzbar.pyzbar import decode
 
 # Load an image with a barcode
-image = cv2.imread('path_to_your_barcode_image.jpg')  # Replace with the path to an image
+image = cv2.imread('/home/amruta/Documents/Attendance/attendance_barcode/image.png')  # Replace with the path to an image
 
 # Decode the barcode
 decoded_objects = decode(image)
